17d6a8329247ca0d36a8b9769e2eea2230ba17cc
Hamed166 <hamed.mgt.cu@gmail.com>
2021-08-20 14:27:08 +0600

    first commit

---
+++ cart.js
@@ -0,0 +1,12 @@
+
+function 
+
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    const caseNumber = document.getElementById('case-number');
+    const caseInput = caseNumber.value;
+    caseNumber.value = parseInt(caseInput) + 1;
+})
+
+document.getElementById()
+
