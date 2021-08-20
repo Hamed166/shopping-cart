8fd8238bc47fb223fcadbe1dcc1c4550b5b11432
Hamed166 <hamed.mgt.cu@gmail.com>
2021-08-20 14:39:54 +0600

    case number fun

--- cart.js
+++ cart.js
@@ -1,12 +1,19 @@
 
-function 
-
-
-document.getElementById('case-plus').addEventListener('click', function(){
+ 
+function updateTotalCase(){
     const caseNumber = document.getElementById('case-number');
     const caseInput = caseNumber.value;
     caseNumber.value = parseInt(caseInput) + 1;
+}
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateTotalCase();
 })
 
-document.getElementById()
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseNumber = document.getElementById('case-number');
+    const caseInput = caseNumber.value;
+    caseNumber.value = parseInt(caseInput) - 1;
+
+})
 
